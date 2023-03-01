64994892e4f43c95e74c2ba1ca98d008c9f5c6a0
Yoon <yoonminwater@gmail.com>
2023-03-01 22:29:13 +0900

    chore: 프로젝트 초기 환경설정 (#2)

---
+++ backend/src/main/java/com/honeygoods/HoneyGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.honeygoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoneyGoodsApplication.class, args);
+    }
+
+}
