3ae8d51202d98fb525b8cb7b3442a85a3a4556bb
yoonms0617 <yoonminwater@gmail.com>
2023-03-13 10:01:45 +0900

    feat: email로 사용자 조회 메소드 추가

--- backend/src/main/java/com/honeygoods/member/repository/MemberRepository.java
+++ backend/src/main/java/com/honeygoods/member/repository/MemberRepository.java
@@ -4,7 +4,11 @@ import com.honeygoods.member.domain.Member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
 
     boolean existsByNickname(String nickname);
 
